50afe24e9f1efed1ea56c304ff0a0ff92053032c
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-05-11 14:52:10 +0530

    Create 13.Minimumwindowsort.java

---
+++ Arrays/01.TwoPointers/13.Minimumwindowsort.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+
+        int n = nums.length;
+        if(n<=1) return 0;
+
+        int left = -1;
+        int right = -1;
+
+        
+        int max = nums[0];
+
+        for (int i = 1; i < n; i++) {
+
+            if (nums[i] < max) {
+                right = i;
+            } else {
+                max = nums[i];
+            }
+        }
+
+        if (right == -1) {
+            return 0;
+        }
+
+        int min = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (nums[i] > min) {
+                left = i;
+            } else {
+                min = nums[i];
+            }
+        }
+
+        return right - left + 1;
+    }
+}
