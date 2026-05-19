b38e064de64349dadbfa8a7edb7f50b8609109f2
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-05-19 07:10:53 +0530

    Update 04.FindDuplicate.java

--- LinkedList/01.SlowFastPointer/04.FindDuplicate.java
+++ LinkedList/01.SlowFastPointer/04.FindDuplicate.java
@@ -1,3 +1,28 @@
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])) return nums[i];
+            set.add(nums[i]);
+        }
+        return -1;
+    }
+}
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]) return nums[i];
+        }
+        return -1;
+    }
+}
+
+
+
+
 class Solution {
     public int findDuplicate(int[] nums) {
         int slow = nums[0];
