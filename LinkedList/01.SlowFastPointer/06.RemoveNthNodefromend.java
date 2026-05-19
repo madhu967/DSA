6813dc30dd838fc133773acf9a90186ae04c92bc
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-05-19 07:53:10 +0530

    Update 06.RemoveNthNodefromend.java

--- LinkedList/01.SlowFastPointer/06.RemoveNthNodefromend.java
+++ LinkedList/01.SlowFastPointer/06.RemoveNthNodefromend.java
@@ -30,3 +30,31 @@ class Solution {
         return head;
     }
 }
+
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        if (fast == null) {
+            return head.next;
+        }
+
+        while (fast != null) {
+            fast = fast.next;
+            prev = slow;
+            slow = slow.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+    }
+}
