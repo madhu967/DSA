a63d8ca4d4b8a5e4786d19156f63d9131fad7b46
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-05-19 07:08:32 +0530

    Create 04.FindDuplicate.java

---
+++ LinkedList/01.SlowFastPointer/04.FindDuplicate.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        while (fast < nums.length && nums[fast] < nums.length) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if (slow == fast) {
+                slow = nums[0];
+
+                while (slow != fast) {
+                    slow = nums[slow];
+                    fast = nums[fast];
+                }
+
+                return slow;
+            }
+        }
+
+        return -1;
+    }
+}
