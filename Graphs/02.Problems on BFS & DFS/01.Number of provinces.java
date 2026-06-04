5815a4e542225d4ae861899a6359402f63587687
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-06-04 07:35:15 +0530

    Update 01.Number of provinces.java

--- Graphs/02.Problems on BFS & DFS/01.Number of provinces.java
+++ Graphs/02.Problems on BFS & DFS/01.Number of provinces.java
@@ -1,1 +1,36 @@
-Number of provinces
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 & i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
+        boolean[] visited=new boolean[n];
+        int provinces=0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                dfs(i,adj,visited);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+
+    private void dfs(int node,List<List<Integer>> adj,boolean[] visited){
+        visited[node]=true;
+        for(int neighbour:adj.get(node)){
+            if(!visited[neighbour]){
+                dfs(neighbour,adj,visited);
+            }
+        }
+    }
+   
+}
