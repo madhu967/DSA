83db1efe7e9fb5fdd1cc38e24a71e5598f78cb3b
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-06-10 07:46:53 +0530

    Update 01.PrintNameNTimes.java

--- Recursion/01.BasicRecursion(Linear)/01.PrintNameNTimes.java
+++ Recursion/01.BasicRecursion(Linear)/01.PrintNameNTimes.java
@@ -1,1 +1,14 @@
 
+class Solution {
+
+    static void printName(int n) {
+        if (n == 0) return;   
+
+        System.out.println("Madhu"); 
+        printName(n - 1);   
+    }
+
+    public static void main(String[] args) {
+        printName(5);
+    }
+}
