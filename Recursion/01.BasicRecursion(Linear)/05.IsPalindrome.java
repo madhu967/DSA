86a9eae45f1d72d9214d439c590b4a63521c0948
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-06-10 16:47:52 +0530

    Create 05.IsPalindrome.java

---
+++ Recursion/01.BasicRecursion(Linear)/05.IsPalindrome.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public static boolean isPalindrome(String s, int left, int right) {
+        
+        if (left >= right) {
+            return true;
+        }
+
+        if (s.charAt(left) != s.charAt(right)) {
+            return false;
+        }
+
+        return isPalindrome(s, left + 1, right - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+
+        System.out.println(isPalindrome(str, 0, str.length() - 1));
+    }
+}
