9482a95df9dff57a559c18c343816e49aaf9bcc5
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-06-10 07:51:10 +0530

    Create 02.Print1ToN.java

---
+++ Recursion/01.BasicRecursion(Linear)/02.Print1ToN.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    static void print1ToN(int n) {
+        if (n == 0) return;   
+
+        print1ToN(n - 1);     
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        print1ToN(5);
+    }
+}
