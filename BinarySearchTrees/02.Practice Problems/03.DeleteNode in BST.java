91ca26a60e7bc6f23b9e7ec42a8ecfe86f6ba3d4
Ijji Madhu Venkat <156585768+madhu967@users.noreply.github.com>
2026-06-12 11:55:32 +0530

    Create 03.DeleteNode in BST.java

---
+++ BinarySearchTrees/02.Practice Problems/03.DeleteNode in BST.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+        if (root == null) return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        }
+        else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        }
+        else {
+
+            // No left child
+            if (root.left == null)
+                return root.right;
+
+            // No right child
+            if (root.right == null)
+                return root.left;
+
+            // Two children
+            TreeNode temp = root.right;
+
+            while (temp.left != null) {
+                temp = temp.left;
+            }
+
+            root.val = temp.val;
+
+            root.right = deleteNode(root.right, temp.val);
+        }
+
+        return root;
+    }
+}
